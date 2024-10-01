5894140812e178324bb6d5eb3edb1f36f8552fe9
Carl Jones <carljones.workemail@gmail.com>
2024-10-01 11:07:01 -0700

    Update TaskList.js
    10-1-2024 added conditional class name to apply styles based on task completion status

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,4 +1,5 @@
 // components/TaskList.js
+// 10-1-2024 added conditional class name to apply styles based on task completion status. 
 import React from 'react';
 import styles from '../styles/TaskTracker.module.css';
 
@@ -6,14 +7,14 @@ const TaskList = ({ tasks, onToggle }) => {
   return (
     <ul className={styles.taskTrackerList}>
       {tasks.map((task, index) => (
-        <li key={index} className={styles.taskTrackerItem}>
+        <li key={index} className={`${styles.taskTrackerItem} ${task.completed ? styles.completed : ''}`}>
           <input
             type="checkbox"
             checked={task.completed}
             onChange={() => onToggle(index)}
             className={styles.taskTrackerCheckbox}
           />
-          <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+          <span className={task.completed ? styles.completedText : ''}>
             {task.name}
           </span>
         </li>
